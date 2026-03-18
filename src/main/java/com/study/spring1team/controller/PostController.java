263f722f0e2a61a4f6a76ad2c22d3237024f7899
bsh-ko <ssuh3872@gmail.com>
2026-03-18 17:48:43 +0900

    feat: 게시글 dto 생성

---
+++ src/main/java/com/study/spring1team/controller/PostController.java
@@ -0,0 +1,17 @@
+package com.study.spring1team.controller;
+
+import com.study.spring1team.global.apiPayload.ApiResponse;
+import com.study.spring1team.dto.PostRequestDTO;
+import com.study.spring1team.global.apiPayload.code.GeneralSuccessCode;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    @PostMapping("/")
+    public ApiResponse<String> createPost(@Valid @RequestBody PostRequestDTO request) {
+        return ApiResponse.onSuccess(GeneralSuccessCode.OK, "게시글이 성공적으로 생성되었습니다.");
+    }
+}
