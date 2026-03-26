5a8848ef3aba73aa4e8369c515dc21ab99f060f4
bsh-ko <ssuh3872@gmail.com>
2026-03-26 11:40:58 +0900

    feat: 비밀번호 필드 추가

--- src/main/java/com/study/spring1team/domain/user/entity/User.java
+++ src/main/java/com/study/spring1team/domain/user/entity/User.java
@@ -18,6 +18,9 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 100)
+    private String password;
+
     @Column(nullable = false, length = 20)
     private String name;
 
